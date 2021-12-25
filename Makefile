ffc81d4555c403cd5de70e4512ae8a35c2dc6402
ArenorRaxx <mateocorso@gmail.com>
2021-12-25 16:38:48 +0100

    adding utils ft_putnbr_base.c

--- Makefile
+++ Makefile
@@ -6,13 +6,14 @@
 #    By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/12/25 12:06:45 by mcorso            #+#    #+#              #
-#    Updated: 2021/12/25 15:57:58 by mcorso           ###   ########.fr        #
+#    Updated: 2021/12/25 16:38:27 by mcorso           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SRCS = ./srcs/ft_printf.c\
 		./utils/ft_putchar.c\
-		./utils/ft_putstr.c
+		./utils/ft_putstr.c\
+		./utils/ft_putnbr_base.c
 OBJ = ${SRCS:.c=.o}
 HEAD = libftprintf.h
 NAME = libftprintf.a
--- utils/ft_putnbr_base.c
+++ utils/ft_putnbr_base.c
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/24 12:44:27 by mcorso            #+#    #+#             */
-/*   Updated: 2021/12/25 16:37:58 by mcorso           ###   ########.fr       */
+/*   Created: 2021/12/25 16:00:43 by mcorso            #+#    #+#             */
+/*   Updated: 2021/12/25 16:37:25 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "../libftprintf.h"
 
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
-	int 	ft_printf(const char *format, ...);
-	int		is_in_set(char c, const char *charset);
-	int		ft_print_format(const char **format, va_list args);
-	void	ft_putchar(char c);
-	void	ft_putstr(char *str);
-	void	ft_format(char c, va_list args);
-	void	ft_putnbr_base(int num, char *base);
-#endif
+void	ft_putnbr_base(int num, char *base)
+{
+	static int	lenbase;
+
+	if (!lenbase)
+		while (base[++lenbase])
+			;
+	if (num / lenbase != 0)
+		ft_putnbr_base(num / lenbase, base);
+	ft_putchar(base[num % lenbase]);
+}
+
+int	main(void)
+{
+	ft_putnbr_base(16, "0123456789ABCDEF");
+}
